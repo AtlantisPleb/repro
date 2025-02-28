a63de261ffcecb8c6d4d1caa46685d2b56487e70
Christopher David <chris@openagents.com>
2025-02-28 09:51:40 -0600

    Configure drizzlee

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: "11c66c65-23f6-4ab1-96b1-38c085ca4fa2",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
